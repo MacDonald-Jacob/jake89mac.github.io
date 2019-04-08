7984585804d9ad493b41b6929a04346b9800cc3b
Jacob MacDonald Account <mac17041@byui.edu>
2019-04-08 11:45:46 -0600

    hk

--- TempleSite/scripts/weatherHongKong.js
+++ TempleSite/scripts/weatherHongKong.js
@@ -1,6 +1,6 @@
 let weatherRequest3 = new XMLHttpRequest();
 
-weatherRequest3.open('GET','http://api.openweathermap.org/data/2.5/weather?id=1819414&appid=44ae659aedc9547e507e6cd8e21c0f5a&units=imperial', true);
+weatherRequest3.open('GET','//api.openweathermap.org/data/2.5/weather?id=1819414&appid=44ae659aedc9547e507e6cd8e21c0f5a&units=imperial', true);
 
 weatherRequest3.send();
 
